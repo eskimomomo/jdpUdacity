71b9a74443129e3798435f664a60e222250a9d7d
stephen <stephen@zamano.net>
2018-05-23 15:21:12 +0100

    requirements2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var elem = document.getElementById('catPic');
+var elem = document.getElementById('catPic1');
 elem.addEventListener('click', function(){
-    totalClicks = Number(document.getElementById('totalClicks').textContent);
-    document.getElementById('totalClicks').textContent = ++totalClicks;
+    totalClicks = Number(document.getElementById('totalClicks1').textContent);
+    document.getElementById('totalClicks1').textContent = ++totalClicks;
 }, false);
+
+
+var elem = document.getElementById('catPic2');
+elem.addEventListener('click', function(){
+    totalClicks = Number(document.getElementById('totalClicks2').textContent);
+    document.getElementById('totalClicks2').textContent = ++totalClicks;
+}, false);
+
+document.getElementById('catName2').textContent = 'Fred';
+document.getElementById('catName1').textContent = 'Garfield';
