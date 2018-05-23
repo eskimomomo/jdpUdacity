5dd6b10c525fcf0b8214323feec73296df2875a7
stephen <stephen@zamano.net>
2018-05-23 16:23:08 +0100

    cat clicker with 5 cats

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-var elem = document.getElementById('catPic1');
-elem.addEventListener('click', function(){
-    totalClicks = Number(document.getElementById('totalClicks1').textContent);
-    document.getElementById('totalClicks1').textContent = ++totalClicks;
-}, false);
+var Cats  = [
+    {catName: 'Fred', imageLocation: 'cat1.jpg', clicks: 0},
+    {catName: 'Garfield', imageLocation: 'cat2.jpg', clicks: 0},
+    {catName: 'Barney', imageLocation: 'cat3.jpg', clicks: 0},
+    {catName: 'Eskimo', imageLocation: 'cat4.jpg' , clicks: 0},
+    {catName: 'Tree', imageLocation: 'cat5.jpg' , clicks: 0}
+]
 
+Cats.forEach(function (elem,index,arr)
+{
+    console.log(elem);
+    $('#catList').append('<a href="javascript:populateCat(' + index + ')">' + elem.catName + '</a></br>');
+});
 
-var elem = document.getElementById('catPic2');
-elem.addEventListener('click', function(){
-    totalClicks = Number(document.getElementById('totalClicks2').textContent);
-    document.getElementById('totalClicks2').textContent = ++totalClicks;
-}, false);
+function populateCat(index)
+{
+    $('#catName').text(Cats[index].catName);
+    $('#catPic').attr('src','images/' + Cats[index].imageLocation);
+    $('#catPic').attr('catIndex',index);
+    $('#totalClicks').text(Cats[index].clicks)
+}
 
-document.getElementById('catName2').textContent = 'Fred';
-document.getElementById('catName1').textContent = 'Garfield';
+$('#catPic').click(function(e) {
+  //the element has been clicked... do stuff here
+  var index = e.target.attributes['catindex'].value;
+  Cats[index].clicks = ++Cats[index].clicks;
+  $('#totalClicks').text(Cats[index].clicks);
+});
+
+populateCat(1);
